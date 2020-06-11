fb67cfd386f3ccddc8f42b3360e578f0bab3d6f2
alchemistmatt <matthew.monroe@pnnl.gov>
2020-06-11 15:58:04 -0700

    Add example output files
    Includes checksum files, manifest files, .dslink files, and upload batch files

---
+++ Docs/ThermoUploadTest_MoTrPAC_LinkFiles/UploadFiles_DCC.bat
@@ -0,0 +1,21 @@
+call cmd /c gsutil -h Content-MD5:GJRthbqNpGL+0lbXEXRQlg== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-ETciD-30_2\Angiotensin_325-ETciD-30_2.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/MultiFragmentation/ETciD/Angiotensin_325-ETciD-30_2.raw
+call cmd /c gsutil -h Content-MD5:agHfL/Yk76dSTJBN4H8hCw== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-ETciD-30_1\Angiotensin_325-ETciD-30_1.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/MultiFragmentation/ETciD/Angiotensin_325-ETciD-30_1.raw
+call cmd /c gsutil -h Content-MD5:chsGp+i3PILaobx6zCNb2A== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-ETciD-15\Angiotensin_325-ETciD-15.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/MultiFragmentation/ETciD/Angiotensin_ETciD-15_325.raw
+call cmd /c gsutil cp F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Docs\ThermoUploadTest_MoTrPAC_LinkFiles\PASS1B-06\T70\MultiFragmentation\ETciD\ETciD_MANIFEST.txt gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/MultiFragmentation/ETciD/ETciD_MANIFEST.txt
+
+call cmd /c gsutil -h Content-MD5:djN9/Hn2tpH7M8HkfWKTwg== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-EThcD-30\Angiotensin_325-EThcD-30.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/MultiFragmentation/EThcD/Angiotensin_EThcD-30_325.raw
+call cmd /c gsutil cp F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Docs\ThermoUploadTest_MoTrPAC_LinkFiles\PASS1B-06\T70\MultiFragmentation\EThcD\EThcD_MANIFEST.txt gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/MultiFragmentation/EThcD/EThcD_MANIFEST.txt
+
+call cmd /c gsutil -h Content-MD5:GTl3y+c7ID1mbpb13NpYtw== cp \\proto-11\Lumos01\2018_4\Angiotensin_AllScans_1\Angiotensin_AllScans_1.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/Angiotensin_AllScans_1.raw
+call cmd /c gsutil -h Content-MD5:ajJLa5UsD75ugy3zb5O0ew== cp \\proto-11\Lumos01\2018_4\Angiotensin_AllScans_2\Angiotensin_AllScans_2.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/Angiotensin_AllScans.raw
+call cmd /c gsutil -h Content-MD5:iKn7s78Iq2a8/fO4iCRdLg== cp \\proto-4\VOrbiETD03\2019_2\QC_shew_19_01_R06_15May19_Brandi_Thermo-10408462\QC_shew_19_01_R06_15May19_Brandi_Thermo-10408462.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/QC_shew_19_01_R06_15May19.raw
+call cmd /c gsutil cp F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Docs\ThermoUploadTest_MoTrPAC_LinkFiles\PASS1B-06\T70\T70_MANIFEST.txt gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/T70_MANIFEST.txt
+call cmd /c gsutil cp F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Docs\ThermoUploadTest_MoTrPAC_LinkFiles\PASS1B-06\T70\CIDorHCD\CIDorHCD_MANIFEST.txt gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/CIDorHCD/CIDorHCD_MANIFEST.txt
+call cmd /c gsutil cp F:\Documents\Projects\DataMining\DMS_Programs\DMS_Dataset_Retriever\Docs\ThermoUploadTest_MoTrPAC_LinkFiles\PASS1B-06\T70\ETD\ETD_MANIFEST.txt gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/ETD/ETD_MANIFEST.txt
+
+call cmd /c gsutil -h Content-MD5:4z7LZJYB4PVzb9k07ned6Q== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-ETD_2\Angiotensin_325-ETD_2.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/ETD/Angiotensin_325-ETD2.raw
+call cmd /c gsutil -h Content-MD5:oKBPoYF/pmJsrtJCozzV9Q== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-ETD_1\Angiotensin_325-ETD_1.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/ETD/Angiotensin_325-ETD1.raw
+
+call cmd /c gsutil -h Content-MD5:7GEO/kDj/T6IfOQBMCQgRQ== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-HCD\Angiotensin_325-HCD.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/CIDorHCD/Angiotensin_325-HCD.raw
+call cmd /c gsutil -h Content-MD5:+hgDm04mOxmqzM3BlrA3zg== cp \\proto-11\Lumos01\2018_4\Angiotensin_325-CID\Angiotensin_325-CID.raw gs://motrpac-portal-transfer-pnnl/PASS1B-06/T70/CIDorHCD/Angiotensin_325-CID.raw
+
