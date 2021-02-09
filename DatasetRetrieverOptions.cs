1602e1f1d5fb15a681db789cc3594c38274d330b
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-09 10:46:48 -0800

    Remove reference to SharpZipLib

--- DatasetRetrieverOptions.cs
+++ DatasetRetrieverOptions.cs
@@ -15,7 +15,7 @@ namespace DMSDatasetRetriever
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "January 14, 2021";
+        public const string PROGRAM_DATE = "February 9, 2021";
 
         #region "Enums"
 
